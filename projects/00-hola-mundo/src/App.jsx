7bc9b28c3260f2b551547fae5353f959dc59a4ec
Kevin Pelozo <kevindp2008@gmail.com>
2025-05-09 11:56:34 -0600

    Update identation

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -2,12 +2,23 @@ import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
 
 export function App (){
+
   return (
     <section className='tw-section'>
-      <TwitterFollowCard isFollowing userName="midudev" name="Miguel Ángel Durán" />
-      <TwitterFollowCard isFollowing={false} userName="pheralb" name="Pablo Hernández" />
-      <TwitterFollowCard isFollowing={false} userName="elonmusk" name="Elon Musk" />
-      <TwitterFollowCard isFollowing userName="pelozok" name="Kevin Daniel Pelozo" />
+      <TwitterFollowCard 
+        isFollowing 
+        userName="midudev" 
+        name="Miguel Ángel Durán" />
+
+      <TwitterFollowCard 
+        isFollowing={false} 
+        userName="pheralb" 
+        name="Pablo Hernández" />
+        
+      <TwitterFollowCard 
+        isFollowing 
+        userName="pelozok" 
+        name="Kevin Daniel Pelozo" />
     </section>
   )
 }
--- projects/00-hola-mundo/src/TwitterFollowCard.jsx
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -1,22 +1,23 @@
 export function TwitterFollowCard({ userName, name, isFollowing}) {
-    return (
-        <article className='tw-followCard'>
-            <header className='tw-followCard-header'>
-                <img
-                    className='tw-followCard-avatar' 
-                    alt={`avatar de ${userName}`}
-                    src={`https://unavatar.io/${userName}`} />
-                <div className='tw-followCard-info'>
-                    <strong>{name}</strong>
-                    <span className='tw-followCard-infoUserName'>@{userName}</span>
-                </div>
-            </header>
 
-            <aside>
-                <button className='tw-followCard-button'>
-                    Seguir
-                </button>
-            </aside>
-        </article>
-    )
+  return (
+    <article className='tw-followCard'>
+      <header className='tw-followCard-header'>
+        <img
+          className='tw-followCard-avatar' 
+          alt={`avatar de ${userName}`}
+          src={`https://unavatar.io/${userName}`} />
+        <div className='tw-followCard-info'>
+          <strong>{name}</strong>
+          <span className='tw-followCard-infoUserName'>@{userName}</span>
+        </div>
+      </header>
+
+      <aside>
+        <button className='tw-followCard-button'>
+          Seguir
+        </button>
+      </aside>
+    </article>
+  )
 }
